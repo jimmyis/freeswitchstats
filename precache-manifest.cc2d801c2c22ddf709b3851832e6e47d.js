129d40802735ba882bcc2e1ef38af227dd319f1e
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2018-11-15 13:03:29 +0700

    init

---
+++ precache-manifest.cc2d801c2c22ddf709b3851832e6e47d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "764895d021348bba0d2d269929fbd476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdffcca76678ef50adde",
+    "url": "/assets/js/chunk-vendors.993571f7.js"
+  },
+  {
+    "revision": "a661bcc0f5da4afc9d60",
+    "url": "/assets/js/app.b5f31c1b.js"
+  },
+  {
+    "revision": "e7c2b3851ab3965b1ed8",
+    "url": "/assets/js/about.b8cabc56.js"
+  },
+  {
+    "revision": "bdffcca76678ef50adde",
+    "url": "/assets/css/chunk-vendors.c909a6f6.css"
+  },
+  {
+    "revision": "a661bcc0f5da4afc9d60",
+    "url": "/assets/css/app.98af173d.css"
+  }
+];
